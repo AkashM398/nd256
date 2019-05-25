4c1dc1d11ca34bf2164c5e18b3d1681104ff5236
Collin Mutembei <collin.mutembei@andela.com>
2019-05-25 14:26:03 +0300

    Refactor Task2
    - Simplify nested for loop

--- Task2.py
+++ Task2.py
@@ -22,24 +22,13 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
-call_durations = []
-callers = []
-recipients = []
-for call in calls:
-    callers.append(call[0])
-    recipients.append(call[1])
-    call_durations.append(int(call[3]))
-
 number_durations = { number: 0 for number in tel_numbers() }
 
-for number in tel_numbers():
-    for caller_index, caller in enumerate(callers):
-        if number == caller:
-            number_durations[number] += call_durations[caller_index]
+for number in number_durations.keys():
+    for index, call in enumerate(calls):
+        if number == call[0] or number == call[1]:
+            number_durations[number] += int(call[3])
 
-    for recipient_index, recipient in enumerate(recipients):
-        if number == recipient:
-            number_durations[number] += call_durations[recipient_index]
 
 longest_call_duration = max(number_durations.values())
 longest_call_duration_by = list(number_durations.keys())[list(number_durations.values()).index(longest_call_duration)]
