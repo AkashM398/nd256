c23a1f974169e15f982347b70a6a5621a9b8303a
Collin Mutembei <collin.mutembei@andela.com>
2019-06-04 14:20:40 +0300

    Refactor LRU_Cache implementation

--- Show Me the Data Structures/lru_cache.py
+++ Show Me the Data Structures/lru_cache.py
@@ -1,42 +1,47 @@
 class DoubleLinkedListNode(object):
-    def __init__(self, key, value):
-        self.key = key
+    def __init__(self, value):
         self.value = value
         self.next = None
         self.previous = None
 
+    def __repr__(self):
+        return str(self.value)
+
+
 class LRU_Cache(object):
 
-    def __init__(self, capacity=0):
-        # Initialize class variables
+    def __init__(self, capacity: int):
+        self._cache = dict()
         self.capacity = capacity
-        self.cache = dict()
-        self.head = DoubleLinkedListNode(0, 0)
-        self.tail = DoubleLinkedListNode(0, 0)
+        self.head = DoubleLinkedListNode(0)
+        self.tail = DoubleLinkedListNode(0)
         self.head.next = self.tail
         self.tail.previous = self.head
 
     def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent. 
-        if key in self.cache:
-            node = self.cache.get(key)
-            self._refresh(node)
-            return node.value
+        if key in self._cache:
+            node = self._cache.get(key)
+            self._remove(node)
+            self._add(node)
+            return node
+
         return -1
 
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
-        if key in self.cache:
+        if key in self._cache:
             self._remove(self.cache.get(key))
-        elif len(self.cache) > self.capacity:
+
+        node = DoubleLinkedListNode(value)
+        self._add(node)
+        self._cache[key] = node
+
+        if len(self._cache) > self.capacity:
             node = self.head.next
             self._remove(node)
-            del self.cache[node.key]
-        else:
-            node = DoubleLinkedListNode(key, value)
-            self._add(node)
-            self.cache[key] = node
+            node_key = list(self._cache.keys())[list(self._cache.values()).index(node)]
+            del self._cache[node_key]
 
+       
     def _add(self, node):
         last_node = self.tail.previous
         last_node.next = node
@@ -50,15 +55,27 @@ class LRU_Cache(object):
         previous_node.next = next_node
         next_node.previous = previous_node
 
-    def _refresh(self, node):
-        self._remove(node)
-        self._add(node)
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    def __len__(self):
+        return len(self._cache)
+
+    def __repr__(self):
+        cache_items = [node.value for node in self][1:-1]
+        cache_items.reverse()
+        return str(cache_items)
 
 
-our_cache = LRU_Cache(5)
-
-our_cache.set(1, 1)
-our_cache.set(2, 2)
-print(our_cache.get(1))       # returns 1
-print(our_cache.get(2))       # returns 2
-print(our_cache.get(3))       # return -1
+our_cache = LRU_Cache(3)
+our_cache.set(1, 1); print(our_cache) # [1]
+our_cache.set(2, 2); print(our_cache) # [2, 1]
+our_cache.set(3, 3); print(our_cache) # [3, 2, 1]
+our_cache.set(4, 4); print(our_cache) # [4, 3, 2]
+our_cache.get(3); print(our_cache) # [3, 4, 2]
+print(our_cache.get(1)) # returns -1
+print(our_cache.get(2)) # returns 2
+print(our_cache.get(3)) # return 3
