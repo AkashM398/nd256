3d44e5ea1158d6a54bb99ffae988d97817816944
Collin Mutembei <collin.mutembei@andela.com>
2019-06-07 19:17:05 +0300

    Add explanation for blockchain solution

--- Show Me the Data Structures/blockchain.py
+++ Show Me the Data Structures/blockchain.py
@@ -1,18 +1,19 @@
 import hashlib
 from time import time
 
+
 class Block:
     def __init__(self, timestamp, data, previous_hash=None):
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
 
     def calc_hash(self):
-      sha = hashlib.sha256()
-      hash_str = str(self.timestamp) + str(self.data) + str(self.previous_hash)
-      sha.update(hash_str.encode('utf-8'))
-      return sha.hexdigest()
+        sha = hashlib.sha256()
+        hash_str = str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        sha.update(hash_str.encode("utf-8"))
+        return sha.hexdigest()
 
 
 class Chain(object):
@@ -24,17 +25,12 @@ class Chain(object):
 
     def add_node(self, data):
         if self.head is None:
-            self.head = Block(
-                timestamp=time(),
-                data=data
-            )
+            self.head = Block(timestamp=time(), data=data)
             self.last_node = self.head
             return
-        
+
         self.last_node.next = Block(
-            timestamp=time(),
-            data=data,
-            previous_hash=self.last_node.hash
+            timestamp=time(), data=data, previous_hash=self.last_node.hash
         )
         self.last_node.next.previous = self.last_node
         self.last_node = self.last_node.next
@@ -48,11 +44,11 @@ class Chain(object):
         else:
             yield self.head.hash
 
-
     def __repr__(self):
         node_list = [n for n in self]
         node_list.reverse()
         return str(node_list)
+
 
 chain = Chain()
 chain.add_node("one")
