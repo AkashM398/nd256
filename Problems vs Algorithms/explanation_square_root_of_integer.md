e4cba2b4efe53559e37dca271265098fd7684b99
Collin Mutembei <collin.mutembei@andela.com>
2019-06-07 20:00:19 +0300

    Add explanation for square root of integer solution

---
+++ Problems vs Algorithms/explanation_square_root_of_integer.md
@@ -0,0 +1,13 @@
+### Code Design
+
+I implemented my using iterative binary search algorithm. I am setting the `end_number` to be half the input number to reduce the search space for the algorithm. The square root of a number is lesser than half the number. If the input number is 0 or 1, the algorithm terminates early by returning the number.
+
+### Efficiency
+
+##### Time complexity
+
+- It takes logarithmic time to get the result; O(log n)
+
+##### Space Complexity
+
+- The space taken by my solution is not affected by the input; O(1)
--- Problems vs Algorithms/square_root_of_integer.py
+++ Problems vs Algorithms/square_root_of_integer.py
@@ -1,5 +1,5 @@
 def sqrt(number):
-   """
+    """
    Calculate the floored square root of a number
 
    Args:
@@ -7,27 +7,27 @@ def sqrt(number):
    Returns:
    int: Floored Square Root
    """
-   start_number = 1
-   end_number = number // 2
+    start_number = 1
+    end_number = number // 2
 
-   if number == 0 or number == 1:
-      return number
+    if number == 0 or number == 1:
+        return number
 
-   while start_number <= end_number:
-      mid_number = (start_number + end_number) // 2
-      if mid_number * mid_number == number:
-         return mid_number
-      elif mid_number * mid_number < number:
-         start_number = mid_number + 1
-         result = mid_number
-      else:
-         end_number = mid_number - 1
+    while start_number <= end_number:
+        mid_number = (start_number + end_number) // 2
+        if mid_number * mid_number == number:
+            return mid_number
+        elif mid_number * mid_number < number:
+            start_number = mid_number + 1
+            result = mid_number
+        else:
+            end_number = mid_number - 1
 
-   return result
+    return result
 
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
