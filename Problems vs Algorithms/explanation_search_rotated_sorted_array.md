deff0068edca2a54432ae5d9195ecf6f348de9ec
Collin Mutembei <collin.mutembei@andela.com>
2019-06-07 20:15:54 +0300

    Add explanation for search sorted array solution

---
+++ Problems vs Algorithms/explanation_search_rotated_sorted_array.md
@@ -0,0 +1,13 @@
+### Code Design
+
+I implemented my using recursive binary search algorithm as a nested method in the `rotated_array_search` for easier usage of the number variable.
+
+### Efficiency
+
+##### Time complexity
+
+- It takes logarithmic time to get the result; O(log n)
+
+##### Space Complexity
+
+- The space taken by my solution is not affected by the input; O(1)
--- Problems vs Algorithms/search_rotated_sorted_array.py
+++ Problems vs Algorithms/search_rotated_sorted_array.py
@@ -7,6 +7,7 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
+
     def binary_search(start_index=0, end_index=len(input_list) - 1):
         mid_index = (start_index + end_index) // 2
 
@@ -15,12 +16,18 @@ def rotated_array_search(input_list, number):
         elif number == input_list[mid_index]:
             return mid_index
         elif number > input_list[mid_index]:
-            if input_list[mid_index] < input_list[start_index] and number >= input_list[start_index]:
+            if (
+                input_list[mid_index] < input_list[start_index]
+                and number >= input_list[start_index]
+            ):
                 return binary_search(start_index, mid_index - 1)
             else:
                 return binary_search(mid_index + 1, end_index)
         else:
-            if input_list[mid_index] > input_list[end_index] and number <= input_list[end_index]:
+            if (
+                input_list[mid_index] > input_list[end_index]
+                and number <= input_list[end_index]
+            ):
                 return binary_search(mid_index + 1, end_index)
             else:
                 return binary_search(start_index, mid_index - 1)
@@ -28,11 +35,13 @@ def rotated_array_search(input_list, number):
     result = binary_search()
     return result
 
+
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
         if element == number:
             return index
     return -1
+
 
 def test_function(test_case):
     input_list = test_case[0]
@@ -42,8 +51,10 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 8])
 test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 5, 1, 7, 3, 2, 4], 2])
 test_function([[6, 7, 8, 1, 2, 3, 4], 10])
